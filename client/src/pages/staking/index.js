086e0990dc74b4e7f013abeaf0bc0f86b9480e92
PhamHa103 <havoia1@gmail.com>
2022-08-05 17:53:01 +0700

    staking function

--- client/src/pages/staking/index.js
+++ client/src/pages/staking/index.js
@@ -16,14 +16,48 @@ export default function Staking() {
           <img src="images/Asset 8 2.png" alt="" />
           <div id="land-nfts-left">
             <div id="land-nfts">
-              OWN YOUR OLYMPUS LAND NFTs
+              OWN YOUR <br></br> OLYMPUS LAND NFTs
             </div>
-            <div id="god-has-heard-my-call">
-              Own your Olympus Land, Hero and Statue NFTs which can be played in the game once ROI is launched.
+            <div id="land-nfts-text">
+              Own your Olympus Land, Hero and Statue NFTs <br></br> which can be played in the game once ROI is <br></br> launched.
             </div>
+            <button id="land-nfts-button" type="button " >Owning Tutorial</button>
           </div>
         </div>
       </div>
+
+      <div class="content">
+        <div id="content1">
+          <div id="content1-text">
+            <div id="text1">Land Staking Rewards Now Available</div>
+            <div id="text2">Stake your Land NFT to earn passive rewards.</div>
+          </div>
+          <div id="content1-btn" >
+            <button id="content1-button" type="button " >Stake Land</button>
+          </div>
+        </div>
+      </div>
+
+      <div id="content2" >
+        <div id="content2-img" class="brg">
+          <div id="content2-image">
+            <img src="images/281886969_5124306397645497_3470591421062792137_n 2.png" />
+            {/* <img src="images/Rectangle 63.png" alt="" /> */}
+            <div id="content-image-text">Land NFTs</div>
+          </div>
+        </div>
+        <div>
+          <div id="content2-text">
+            <div id="content2-text-detail">
+              Land is the primary NFT asset in Mavia, and is required in order to build a base and to train an army.
+            </div>
+            <button id="content2-button" type="button " >Coming soon</button>
+          </div>
+        </div>
+
+      </div>
+
+
       <Footer />
     </div>
   );
