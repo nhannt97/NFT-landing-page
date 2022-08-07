02b83ec1972c3f210a63941f1b0b8f4e5026fb06
jinfo-nhannguyen <nhan.nguyen@jinformation.net>
2022-08-08 01:35:13 +0700

    loading screen

--- client/src/router.js
+++ client/src/router.js
@@ -11,28 +11,36 @@ import Staking from './pages/staking/index';
 import Building from './pages/building/index';
 import Team from './pages/team/index';
 import Guide from './pages/guide/index';
+import LoadingScreen from "./layouts/loading-screen";
 
 export const routers = [{
   path: '/',
-  component: <Home />
+  component: <Home />,
+  loadTime: 2
 }, {
   path: '/market-place',
   component: <MarketPlace />,
+  loadTime: 2
 }, {
   path: 'about',
   component: <About />,
+  loadTime: 2
 }, {
   path: '/building',
   component: <Building />,
+  loadTime: 2
 }, {
   path: '/staking',
   component: <Staking />,
+  loadTime: 2
 }, {
   path: '/team',
   component: <Team />,
+  loadTime: 2
 }, {
   path: '/guide',
   component: <Guide />,
+  loadTime: 2
 }];
 
 export default function routerRender() {
@@ -40,7 +48,15 @@ export default function routerRender() {
     <Router>
       <Routes>
         {routers.map((router) => (
-          <Route path={router.path} key={router.path} element={router.component}>
+          <Route
+            path={router.path}
+            key={router.path}
+            element={(
+              <LoadingScreen time={router.loadTime}>
+                {router.component}
+              </LoadingScreen>
+            )}
+          >
           </Route>
         ))}
       </Routes>
