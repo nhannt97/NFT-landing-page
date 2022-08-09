e5a85e99f193de6da2712b7e54bf52b09be982ee
PhamHa103 <havoia1@gmail.com>
2022-08-10 03:05:40 +0700

    version 2 done

--- client/src/components/socials/index.js
+++ client/src/components/socials/index.js
@@ -6,14 +6,14 @@ export default function Stores() {
     <div class="socials">
       <div class="socials-title">Follow Rise of Immortals on</div>
       <div>
-        <img src="images/Social/Telegram.png" alt="telegram" />
-        <img src="images/Social/Facebook.png" alt="facebook" />
-        <img src="images/Social/Instagram.png" alt="instagram" />
-        <img src="images/Social/Twitter.png" alt="twitter" />
-        <img src="images/Social/Discord.png" alt="discord" />
-        <img src="images/Social/Youtube.png" alt="youtube" />
-        <img src="images/Social/7.png" alt="" />
-        <img src="images/Social/8.png" alt="" />
+        <a href="https://t.me/RiseofImmortalsOfficial"><img src="images/Social/Telegram.png" alt="telegram" /></a>
+        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Facebook.png" alt="facebook" /></a>
+        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Instagram.png" alt="instagram" /></a>
+        <a href="https://twitter.com/ImmortalsRise"><img src="images/Social/Twitter.png" alt="twitter" /></a>
+        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Discord.png" alt="discord" /></a>
+        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Youtube.png" alt="youtube" /></a>
+        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/7.png" alt="medium" /></a>
+        <a href=""><img src="images/Social/8.png" alt="" /></a>
       </div>
     </div>
   );
