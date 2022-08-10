391240075dbf2ece05e60cd72e2916dcb5496f0e
PhamHa103 <havoia1@gmail.com>
2022-08-10 20:25:06 +0700

    update social link

--- client/src/components/socials/index.js
+++ client/src/components/socials/index.js
@@ -8,12 +8,12 @@ export default function Stores() {
       <div>
         <a href="https://t.me/RiseofImmortalsOfficial"><img src="images/Social/Telegram.png" alt="telegram" /></a>
         <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Facebook.png" alt="facebook" /></a>
-        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Instagram.png" alt="instagram" /></a>
+        <a href="https://www.instagram.com/riseofimmortals/"><img src="images/Social/Instagram.png" alt="instagram" /></a>
         <a href="https://twitter.com/ImmortalsRise"><img src="images/Social/Twitter.png" alt="twitter" /></a>
         <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Discord.png" alt="discord" /></a>
-        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/Youtube.png" alt="youtube" /></a>
+        <a href="https://www.youtube.com/channel/UCpZSWKEYHAfPaoG5LsFm7Cw" ><img src="images/Social/Youtube.png" alt="youtube" /></a>
         <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/7.png" alt="medium" /></a>
-        <a href=""><img src="images/Social/8.png" alt="" /></a>
+        <a href="" data-toggle="tooltip" data-placement="top" title="Up comming"><img src="images/Social/8.png" alt="" /></a>
       </div>
     </div>
   );
